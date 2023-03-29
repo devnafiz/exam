14907e9548d4da4e46dfe3be1d0706e8be80af81
nafiz <nafiz.2011@yahoo.com>
2023-03-29 22:59:41 +0600

    queue send mail

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Howo to  send email</title>
+</head>
+<body>
+	<div>
+		
+		<h2>hello sir</h2>
+		<p>Some third-party email providers such as Mailgun and Postmark support message "tags" and "metadata", which may be used to group and track emails sent by your application. You may add tags and metadata to an email message via your</p>
+	</div>
+
+</body>
+</html>
