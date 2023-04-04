0f39fa81512cb0455af6cc26811880e646a8c25d
nafiz <nafiz.2011@yahoo.com>
2023-04-04 15:28:26 +0600

    addd  bokResource

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'title', 'description'];
+
+    public function users(){
+
+    	return $this->belongsTo(User::class,'user_id','id');
+    }
+
+    public function ratings()
+    {
+      return $this->hasMany(Rating::class);
+    }
 }
